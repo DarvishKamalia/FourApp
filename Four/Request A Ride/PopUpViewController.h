5311aa7d0715f85cb88e75fee4ca60f4c113a47f
saurabhj80 <saurabhj80@gmail.com>
2015-10-10 15:29:04 -0700

    Pop Up VC

---
+++ Four/Request A Ride/PopUpViewController.h
@@ -0,0 +1,15 @@
+//
+//  PopUpViewController.h
+//  Four
+//
+//  Created by Saurabh Jain on 10/10/15.
+//  Copyright © 2015 SCSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopUpViewController : UIViewController
+
+// Pass in the model
+
+@end
