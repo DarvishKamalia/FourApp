f4809f7bf3985c21b7b72e58b92cbbec2e417d6f
Bohui Moon <moondennis@naver.com>
2015-10-10 06:01:06 -0700

    Project start

---
+++ Four/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Four
+//
+//  Created by Bohui Moon on 10/10/15.
+//  Copyright © 2015 SCSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '9.0'
+
+link_with 'Four', 'Four Tests'
+pod 'CocoaLumberjack'
