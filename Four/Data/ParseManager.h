75fe8cba2257677e6f331b778fdb80d4bc07f7ae
Bohui Moon <moondennis@naver.com>
2015-10-10 14:53:03 -0700

    Added ParseManager and method for creating new Ride

---
+++ Four/Data/ParseManager.h
@@ -0,0 +1,22 @@
+//
+//  ParseManager.h
+//  Four
+//
+//  Created by Bohui Moon on 10/10/15.
+//  Copyright © 2015 SCSC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+@interface ParseManager : NSObject
+
++ (instancetype)sharedManager;
+
+- (void)createRideWithStart:(CLLocation *)start
+                destination:(CLLocation *)destination
+                      price:(float)price
+                      seats:(int)seats
+                      block:(void (^)(NSError *error))block;
+
+@end
---
+++ Four/Data/ParseManager.m
@@ -0,0 +1,77 @@
+//
+//  ParseManager.m
+//  Four
+//
+//  Created by Bohui Moon on 10/10/15.
+//  Copyright © 2015 SCSC. All rights reserved.
+//
+
+#import "ParseManager.h"
+
+
+@interface ParseManager()
+
+
+
+@end
+
+
+@implementation ParseManager
+#pragma mark Init
+/**
+ * Returns singleton instance of ParseManager.
+ * This class should not be instantiated locally.
+ */
++ (id)sharedManager
+{
+    static ParseManager *sharedMyManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedMyManager = [[self alloc] init];
+    });
+    return sharedMyManager;
+}
+
+
+#pragma mark - Methods
+/**
+ * Create a new PFObject Ride with the given parameters, and save.
+ */
+- (void)createRideWithStart:(CLLocation *)start destination:(CLLocation *)destination price:(float)price seats:(int)seats
+                      block:(void (^)(NSError *error))block
+{
+    PFGeoPoint *startGP = [PFGeoPoint geoPointWithLocation:start];
+    PFGeoPoint *destGP  = [PFGeoPoint geoPointWithLocation:destination];
+    
+    PFObject *startLoc = [PFObject objectWithClassName:@"Location"];
+    startLoc[@"point"] = startGP;
+    PFObject *destLoc  = [PFObject objectWithClassName:@"Location"];
+    destLoc[@"point"]  = destGP;
+    
+    PFUser *driver = [PFUser currentUser];
+    
+    NSNumber *priceObj = [NSNumber numberWithFloat:price];
+    NSNumber *seatsObj = [NSNumber numberWithInt:seats];
+    
+    PFObject *newRide = [PFObject objectWithClassName:@"Ride"];
+    newRide[@"start"] = startLoc;
+    newRide[@"destination"] = destLoc;
+    newRide[@"driver"] = driver;
+    newRide[@"riders"] = [NSArray array];
+    newRide[@"price"] = priceObj;
+    newRide[@"seatsLeft"] = seatsObj;
+    
+    [newRide saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error)
+    {
+        if (!error)
+        {
+            //error handler;
+            block(error);
+            return;
+        }
+        
+        block(nil);
+    }];
+}
+
+@end
